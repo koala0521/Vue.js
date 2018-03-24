dc32a9fa69daf7201e1fd1a388c66f7f94dcd72c
koala0521 <328762691@qq.com>
2018-03-24 22:39:28 +0800

    删除todo 、编辑todo

--- todo-list/app.js
+++ todo-list/app.js
@@ -19,7 +19,8 @@ let VM = new Vue({
 	data:{
 		list,
 		show:"all",
-		todo:""
+		todo:"",
+		edittingId:null
 	},
 	methods:{
 		unCompleted:function(){
@@ -43,9 +44,55 @@ let VM = new Vue({
 			var index = this.list.indexOf( item );
 			this.list.splice( index , 1 );
 			
+		},
+		editTodo(item){
+			console.log( item );
+			
+			this.edittingId = item.id;
+		},
+		edited(){
+			this.edittingId = "";
 		}
+			
 
+	},
+	// 自定义指令
+	directives:{
+
+		"focus":{
+			update( el , binding ){ 
+				console.log( el , binding );
+				binding.value && el.focus();				
+			}
+
+		}
 	}
 });
+
+/*
+	定义自定义指令：
+
+		1.全局指令：
+
+		// 注册一个全局自定义指令 `v-focus`
+		Vue.directive('focus', {
+		// 当被绑定的元素插入到 DOM 中时……
+		inserted: function (el) {
+			// 聚焦元素
+			el.focus()
+		}
+		})
+
+		2。局部指令
+		directives: {
+		focus: {
+			// 指令的定义
+			inserted: function (el) {
+				el.focus()
+			}
+		}
+		}
+
+*/
 
 
