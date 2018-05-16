342eab6032ecd4e9110e306d82f657f38c523451
koala0521 <328762691@qq.com>
2018-05-16 11:48:27 +0800

    董事会成员

--- wanka-net/src/views/indexStatement.vue
+++ wanka-net/src/views/indexStatement.vue
@@ -1,12 +1,74 @@
 // 首页 > 免责声明
+<style scoped>
+
+    .content{
+        width: 900px;
+        margin: 0 auto;
+    }
+
+</style>
+
 <template>
-    <div>
-        免责声明页面
+    <div class="cont-width" >
+        <div class="ptb20" >
+            <Breadcrumb separator=">" >
+
+                <BreadcrumbItem 
+                    v-for="item , index in BreadcrumbList"
+                    :key="index"
+                    :to="item.path"
+                >
+                    {{ introduce.BreadcrumbList[index].title }}
+                </BreadcrumbItem>
+
+            </Breadcrumb>
+        </div>
+
+        <div class="content" >
+            <Card
+                :bordered="false"
+                dis-hover 
+                class="cotent-item last" 
+            >
+                <p slot="title" >
+                    {{ introduce.title }} 
+                    
+                </p>
+                <div class="card-cont" v-html="introduce.html" >
+
+                </div>
+
+            </Card>
+        </div>
     </div>
 </template>
 <script>
+
+    import { Breadcrumb , BreadcrumbItem } from 'iview';
+    import { Card , Icon } from 'iview';
+
     export default {
-        
+        components:{
+            Breadcrumb,
+            BreadcrumbItem,
+            Card
+        },
+        data(){
+            return {
+                // 面包屑数据
+                BreadcrumbList : [
+                    {
+                        path:'/'
+                    },
+                    {
+                        path:''
+                    }
+                ],
+                // 语言包
+                introduce : this.$t("content")[0].indexStatement,
+
+            }
+        }
     };
     
 </script>
--- wanka-net/src/views/investorBoardMember.vue
+++ wanka-net/src/views/investorBoardMember.vue
@@ -1,19 +1,98 @@
 // 投资者关系 > 董事会成员
+<style scoped>
+
+    .content{
+        width: 900px;
+        margin: 0 auto;
+    }
+    .item-title{
+        line-height: 2em;
+        font-weight: 600;
+        padding-right: 10px;
+        width: 60px;
+        display: inline-block;
+   }
+   .item-summary{
+	   line-height: 2.2em;
+	   font-size: 1em;
+	   padding-bottom: 0.5em;
+   }
+   .items{
+       border-bottom: 1px solid #dddee1;
+   }
+
+</style>
+
 <template>
-  <div  class="cont-width" >
-      
-    <!-- <div>
-        <router-link  to="/investor/one" > 关系1 </router-link>
-        <router-link  to="/investor/two" >  > 关系2 </router-link>
-    </div> -->
-    <h2> 董事会成员 </h2>
-    董事会成员董事会成员董事会成员董事会成员董事会成员
+    <div class="cont-width" >
+        <div class="ptb20" >
+            <Breadcrumb separator=">" >
 
+                <BreadcrumbItem 
+                    v-for="item , index in BreadcrumbList"
+                    :key="index"
+                    :to="item.path"
+                >
+                    {{ memberInfo.BreadcrumbList[index].title }}
+                </BreadcrumbItem>
 
-  </div>
+            </Breadcrumb>
+        </div>
+
+        <div class="content" >
+            <Card
+                :bordered="false"
+                dis-hover 
+                class="cotent-item last" 
+            >
+                <p slot="title" >
+                    {{ memberInfo.title }}                     
+                </p>
+                <div class="card-cont" >
+                    <ul>
+                        <li 
+                            v-for="item , index in memberInfo.memberList " 
+                            :key="index" 
+                             class="ptb10 items" 
+                        >
+                            <span class="item-title" >{{ item.name }} </span> 
+                            <span class="item-summary" >{{ item.JobTitle }}</span>
+                            <p class="ptb10" >{{ item.description }} </p>
+                        </li>
+                    </ul>
+                    <div class="tar ptb30" > {{ memberInfo.updateTime }} </div>
+                </div>
+
+            </Card>
+        </div>
+    </div>
 </template>
 <script>
+    import { Breadcrumb , BreadcrumbItem } from 'iview';
+    import { Card , Icon } from 'iview';
+
     export default{
+        components:{
+            Breadcrumb,
+            BreadcrumbItem,
+            Card
+        },
+        data(){
+            return {
+                // 面包屑数据
+                BreadcrumbList : [
+                    {
+                        path:'/investor'
+                    },
+                    {
+                        path:''
+                    }
+                ],
+                // 语言包
+                memberInfo : this.$t("content")[0].member,
+
+            }
+        }
 
     };
     
