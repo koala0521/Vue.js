ffd8686942e530c3c2503da72c6b91158e5f66c5
koala0521 <328762691@qq.com>
2018-07-03 20:46:00 +0800

    登陆功能测试

---
+++ wanka-qs/webpack.dev.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const merge = require('webpack-merge');
+const webpackBaseConfig = require('./webpack.base.config.js');
+const fs = require('fs');
+
+// 跨域配置测试
+const proxy = require('http-proxy-middleware');
+
+fs.open('./src/config/env.js', 'w', function(err, fd) {
+    const buf = 'export default "development";';
+    fs.write(fd, buf, 0, buf.length, 0, function(err, written, buffer) {});
+});
+
+module.exports = merge(webpackBaseConfig, {
+    devtool: '#source-map',
+    output: {
+        publicPath: '/dist/',
+        filename: '[name].js',
+        chunkFilename: '[name].chunk.js'
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '[name].css',
+            allChunks: true
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendors',
+            filename: 'vendors.js'
+        }),
+        new HtmlWebpackPlugin({
+            filename: '../index.html',
+            template: './src/template/index.ejs',
+            inject: false
+        })
+    ],
+
+    devServer: {
+        proxy: {
+            //匹配发起的请求的 url
+            '/report/*': {
+                // 目标服务器地址
+                target: 'http://dev.api.tongji.so-quick.cn',
+                //路径重写
+                //   pathRewrite: {'^/index' : '/'},
+                changeOrigin: true,
+                secure: false
+            }
+         }
+    },    
+});
