a705ce917324746e46884fdb5d0962f5da7856b4
koala0521 <328762691@qq.com>
2018-07-05 12:26:07 +0800

    基础路由配置

--- static-demo/src/router.js
+++ static-demo/src/router.js
@@ -1,8 +1,78 @@
-const routers = [{
-    path: '/',
-    meta: {
-        title: ''
+const routers = [
+    // 登陆页面
+    {
+        path: '/login',
+        component: (resolve) => require(['./views/login.vue'], resolve),
+        meta: {
+            title: '快应用统计-登陆'
+        },
+        name:'login'
     },
-    component: (resolve) => require(['./views/index.vue'], resolve)
-}];
+    // 登陆后的页面
+    {
+        path: '',
+        meta: {
+            title: '快应用统计'
+        },
+        component: (resolve) => require(['./views/index.vue'], resolve),
+        children:[
+            
+            // 主页
+            {
+                path: 'home',
+                component: (resolve) => require(['./views/app_list.vue'], resolve),
+                name: 'list',
+                meta: {
+                    title: '主页'
+                }                   
+            },
+
+            // 报表
+            {
+                path: 'charts',
+                component: (resolve) => require(['./views/app_charts.vue'], resolve),
+                children: [
+                    // 默认展示趋势分析
+                    {
+                        path: 'trend',
+                        component: (resolve) => require(['./views/app_charts_trend.vue'], resolve),
+                        name: 'trend',
+                        meta: {
+                            title: '报表 - 趋势分析'
+                        }                
+                    },{
+                        path: 'day',
+                        component: (resolve) => require(['./views/app_charts_day.vue'], resolve),
+                        name: 'day',
+                        meta: {
+                            title: '报表 - 今日概况'
+                        }                          
+                    },            
+                    {
+                        path: '',
+                        redirect: {name:'trend'}
+                    }                   
+                ]
+            },
+            // 开发文档
+            {
+                path: 'doc',
+                component: (resolve) => require(['./views/app_doc.vue'], resolve),
+                name: 'doc',
+                meta: {
+                    title: '开发文档'
+                }                
+            },            
+            {
+                path: '',
+                redirect: 'home'
+            }
+        ]
+    },
+    {
+        path: '*',
+        redirect: { name: 'list' }
+    }
+]
+;
 export default routers;
