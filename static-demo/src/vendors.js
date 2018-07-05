fcca886ef356ae5c12cb41a25973a2e9dddb0d8d
koala0521 <328762691@qq.com>
2018-07-05 12:27:02 +0800

    公共模块抽象：用于单独打包

--- static-demo/src/vendors.js
+++ static-demo/src/vendors.js
@@ -1,4 +1,7 @@
+// global module , independent packaging
 import Vue from 'vue';
 import iView from 'iview';
 import VueRouter from 'vue-router';
 import axios from 'axios';
+import Vuex from 'vuex';
+import echarts from 'echarts';
