8a5fad5bfd7f63ba51572176b887e626b7695c5d
koala0521 <328762691@qq.com>
2018-07-10 10:14:28 +0800

    首页

--- static-demo/src/views/index.vue
+++ static-demo/src/views/index.vue
@@ -1,13 +1,46 @@
+<style scoped>
+    #header{
+        position: fixed;
+        height: auto;
+        left: 0;
+        top: 0;
+        width: 100%;
+        transition: 300ms;
+        z-index: 99;
+        background: #fff;
+    }
+    #content-warp{
+
+    }
+    .content-warp{
+        margin-top:91px;
+    }
+    .hide-notice{
+        margin-top: 60px;
+    }
+    .notice{
+        background: #090f29;
+        color: #fff;
+        line-height: 50px;
+        width: 100%; 
+        z-index: 100;
+        /* display: none; */
+    }
+</style>
+
 <template>
     <div>
-        <div class="ptb20 tac" >
-            <router-link class="prl30" to="/home">主页</router-link>
-            <router-link class="prl30" to="/charts">报表</router-link>
-            <router-link class="prl30" to="/doc">文档</router-link>
-            <button @click="logout" class="fr mr30" >退出</button>
+        <div id="header" class="tac" >
+            <div class="menu ptb10">
+                <router-link class="prl30" to="/home">主页</router-link>
+                <router-link class="prl30" to="/charts">报表</router-link>
+                <router-link class="prl30" to="/doc">文档</router-link>
+                <button @click="logout" class="fr mr30" >退出</button>
+            </div>
+            <div class="notice">提醒广大开发者，获取快应用统计数据前，需先集成快应用统计SDK，具体请参阅开发者文档</div>
         </div>
-        <hr>
-        <div class="prl30" >
+        <!-- 关闭提示信息时， 增加类命 hide-notice 即可-->
+        <div id="content-warp" class="prl30 content-warp ptb20" >
             <router-view></router-view>
         </div>
     </div>
