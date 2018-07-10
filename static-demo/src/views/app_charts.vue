daf47070aa3cf48b76ca96e7ed1048ee51f62354
koala0521 <328762691@qq.com>
2018-07-10 10:13:04 +0800

    报表页面

--- static-demo/src/views/app_charts.vue
+++ static-demo/src/views/app_charts.vue
@@ -1,12 +1,36 @@
+<style scoped>
+    .slider-menu{
+        position: fixed;
+        /* top: 60px; */
+        /* height: 100%; */
+        background: #e4e4e4;
+        /* bottom: 0; */
+        padding-bottom: 190px;
+        border-top: 2px solid #f9fafc;
+        width: 150px;
+    }
+    .page-content-cont{
+        margin-left: 130px;
+    }
+</style>
+
 <template>
-    <div>
-        <div class="left">
+
+    <div class="content-wrap content-width relative" >       
+        <div class="slider-menu">
             <h3>left-menu</h3>
-            <router-link to="/charts">趋势分析</router-link>
-            <router-link to="/charts/day">今日概况</router-link>
+            <ul>
+                <li>
+                <router-link to="/charts">趋势分析</router-link>
+            </li>
+                <li>
+                <router-link to="/charts/day">今日概况</router-link>
+            </li>
+            </ul>
         </div>
-        <div class="content">
+        <div class="page-content-cont"  >
             <router-view></router-view>
-        </div>
+        </div>  
     </div>
+
 </template>
