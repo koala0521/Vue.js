fcedd9009bec9241946a1130405bac8588758036
koala0521 <328762691@qq.com>
2018-07-12 20:19:22 +0800

    app 列表页面开发

--- static-demo/src/views/app_list.vue
+++ static-demo/src/views/app_list.vue
@@ -1,6 +1,45 @@
+<style scoped>
+    .page-title{
+        padding: 20px 0;
+        /* color: #a7a7a9; */
+        font-size: 14px;
+        font-weight: 500;
+    }
+    .page-title .title{
+        line-height: 32px;
+        height: 32px;
+    }
+    .add-btn{
+        background: #4286f5;
+    }
+</style>
+
 <template>
     <div>
-        <h3>快应用中心</h3>
+        <div class="page-title row-flex row-flex-space-between" >
+            <div class="title" >
+               <span class="level-II-title">快应用中心</span>            
+                <Poptip 
+                    trigger="hover" 
+                    placement="bottom-start" 
+                    width="300"
+                    class="poptip-wrap"                
+                >
+                    <Icon type="ios-help-outline"></Icon>   
+                    <div slot="content" >
+                        <p>
+                            指定时间内，用户对您快应用的访问情况，及时了解快应用的实时运营效果
+                        </p>
+                        <p class="mt10" >
+                            更新频率： 今日数据，每10分钟更新一次；昨天以前数据，每天9点后出数据，每天更新一次
+                        </p>
+                    </div>
+                </Poptip>  
+            </div>
+            <div class="add-btn-wrap">                 
+                <Button class="add-btn" type="primary" icon="plus" >创建快应用</Button>
+            </div> 
+        </div>
 
         <!-- <Steps :current="1">
             <Step title="已完成" content="这里是该步骤的描述信息"></Step>
@@ -15,12 +54,12 @@
     </div>
 </template>
 <script>
-    import { Steps , Step } from 'iview';
+    import { Steps , Step , Poptip , Icon , Button } from 'iview';
     import mTbale from '../components/table';
 
     export default {
         components:{
-            Steps , Step , mTbale
+            Steps , Step , mTbale , Poptip , Icon , Button
         },
         data(){
             return {
@@ -36,6 +75,21 @@
                     {
                         title: 'Address',
                         key: 'address'
+                    },{
+                        title: '操作',
+                        key: 'do',
+                        render: (h, params) => {
+                            console.log('params' , params );
+                            
+                            return h('router-link', {
+                                    props: {
+                                        to: '/charts/trend',
+                                    },
+                                    attrs:{
+                                       class:'look-more' 
+                                    }
+                                },'查看详情');
+                        }
                     }
                 ],
                 'list':[
